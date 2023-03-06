42d2b5bb797cb0e22b4a8b03886daee837438acf
Lucas Taqueuti <100800505+Taqueuti@users.noreply.github.com>
2023-03-06 18:57:50 -0300

    Adicionando mais alert

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const medidor = document.querySelector(".medidor");
 const agua = document.querySelector(".agua");
 const quantidade = document.querySelector(".quantidade");
+const meta = 2000;
+let alerta;
 
 let mlConsumidos = 0;
 
+
+/* adcionar água */
 function adicionarAgua(ml) {
   mlConsumidos += ml;
   quantidade.innerText = `${mlConsumidos} ml`;
   const percentualConsumido = (mlConsumidos / 2000) * 100;
   agua.style.height = `${percentualConsumido}%`;
 
-
-  if(mlConsumidos == 2000){
-    const alerta = document.createElement('div');
+  
+  if (mlConsumidos >= meta) {
+    if (alerta) {
+      alerta.remove();
+    }
+    alerta = document.createElement('div');
     alerta.classList.add('alert', 'alert-success', 'my-4');
     alerta.textContent = 'Parabéns! Você alcançou sua meta de consumo de água!';
     document.body.appendChild(alerta);
+  } else if (mlConsumidos > 0) {
+    if (alerta) {
+      alerta.remove();
+    }
+    alerta = document.createElement('div');
+    alerta.classList.add('alert', 'alert-info', 'my-4');
+    alerta.textContent = `Você bebeu ${mlConsumidos}ml de água até agora!`;
+    document.body.appendChild(alerta);
+  } else {
+    if (alerta) {
+      alerta.remove();
+    }
   }
- 
 }
 
+/* zerar medidor */
 function zerarMedidor() {
   mlConsumidos = 0;
   quantidade.innerText = "0 ml";
